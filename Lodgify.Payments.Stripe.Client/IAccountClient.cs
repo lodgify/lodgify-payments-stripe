821244f5b992382d217e07927f07aac1fca55f8f
Marcin Niesyn <marcin.niesyn@gmail.com>
2024-09-09 16:15:41 +0200

    [PAYM-134] Refactor: Split IPaymentStripeClient into IAccountClient and IAccountSessionClient
    Separated the IPaymentStripeClient interface into IAccountClient and IAccountSessionClient for better cohesion and single responsibility principle. This change organizes the code more logically by grouping related methods into distinct interfaces.

--- Lodgify.Payments.Stripe.Client/IAccountClient.cs
+++ Lodgify.Payments.Stripe.Client/IAccountClient.cs
@@ -8,7 +8,7 @@ using Lodgify.Payments.Stripe.Client.Constans;
 namespace Lodgify.Payments.Stripe.Client;
 
 [HttpClient(ClientName = PaymentStripeConstans.HttpClientName, MetricsPrefix = PaymentStripeConstans.MetricsPrefix, UseResponseWrapper = false)]
-public interface IPaymentStripeClient
+public interface IAccountClient
 {
     [ApiEndpoint("POST", "/api/v1/accounts", Name = "CreateAccount")]
     Task<CreateAccountResponse> CreateAccountAsync(
@@ -25,14 +25,5 @@ public interface IPaymentStripeClient
         int propertyOwnerId,
         [HeaderValue(PaymentStripeConstans.SubOwnerIdHeaderName)]
         int? subOwnerId,
-        CancellationToken cancel);
-
-    [ApiEndpoint("POST", "/api/v1/account-sessions", Name = "CreateAccountSession")]
-    Task<CreateAccountSessionResponse> CreateAccountSessionAsync(
-        [HeaderValue(PaymentStripeConstans.PropertyOwnerIdHeaderName)]
-        int propertyOwnerId,
-        [HeaderValue(PaymentStripeConstans.SubOwnerIdHeaderName)]
-        int? subOwnerId,
-        [RequestBody] CreateAccountSessionRequest request,
         CancellationToken cancel);
 }
---
+++ Lodgify.Payments.Stripe.Client/IAccountSessionClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Lodgify.Architecture.HttpClient.Attributes;
+using Lodgify.Payments.Stripe.Api.Models.Requests;
+using Lodgify.Payments.Stripe.Api.Models.Responses;
+using Lodgify.Payments.Stripe.Client.Constans;
+
+namespace Lodgify.Payments.Stripe.Client;
+
+[HttpClient(ClientName = PaymentStripeConstans.HttpClientName, MetricsPrefix = PaymentStripeConstans.MetricsPrefix, UseResponseWrapper = false)]
+public interface IAccountSessionClient
+{
+    [ApiEndpoint("POST", "/api/v1/account-sessions", Name = "CreateAccountSession")]
+    Task<CreateAccountSessionResponse> CreateAccountSessionAsync(
+        [HeaderValue(PaymentStripeConstans.PropertyOwnerIdHeaderName)]
+        int propertyOwnerId,
+        [HeaderValue(PaymentStripeConstans.SubOwnerIdHeaderName)]
+        int? subOwnerId,
+        [RequestBody] CreateAccountSessionRequest request,
+        CancellationToken cancel);
+}
