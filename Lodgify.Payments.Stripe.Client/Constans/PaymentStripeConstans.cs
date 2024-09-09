6b8ffb5d20d30204f05b3767e8ff80512b5f6697
Marcin Niesyn <marcin.niesyn@gmail.com>
2024-09-09 15:41:28 +0200

    [PAYM-134] Add packaging properties and new constant SubOwnerIdHeaderName
    Updated project files to include packaging properties for NuGet package generation. Added SubOwnerIdHeaderName to PaymentStripeConstans and integrated its usage in relevant methods of IPaymentStripeClient.

--- Lodgify.Payments.Stripe.Client/Constans/PaymentStripeConstans.cs
+++ Lodgify.Payments.Stripe.Client/Constans/PaymentStripeConstans.cs
@@ -5,4 +5,5 @@ public class PaymentStripeConstans
     public const string HttpClientName = "LodgifyPaymentsStripe";
     internal const string MetricsPrefix = "lodgify_payments_stripe_client";
     internal const string PropertyOwnerIdHeaderName = "X-PropertyOwnerId";
+    internal const string SubOwnerIdHeaderName = "X-SubOwnerId";
 }
--- Lodgify.Payments.Stripe.Client/IPaymentStripeClient.cs
+++ Lodgify.Payments.Stripe.Client/IPaymentStripeClient.cs
@@ -4,6 +4,7 @@ using Lodgify.Architecture.HttpClient.Attributes;
 using Lodgify.Payments.Stripe.Api.Models.Requests;
 using Lodgify.Payments.Stripe.Api.Models.Responses;
 using Lodgify.Payments.Stripe.Client.Constans;
+
 namespace Lodgify.Payments.Stripe.Client;
 
 [HttpClient(ClientName = PaymentStripeConstans.HttpClientName, MetricsPrefix = PaymentStripeConstans.MetricsPrefix, UseResponseWrapper = false)]
@@ -13,6 +14,8 @@ public interface IPaymentStripeClient
     Task<CreateAccountResponse> CreateAccountAsync(
         [HeaderValue(PaymentStripeConstans.PropertyOwnerIdHeaderName)]
         int propertyOwnerId,
+        [HeaderValue(PaymentStripeConstans.SubOwnerIdHeaderName)]
+        int? subOwnerId,
         [RequestBody] CreateAccountRequest request,
         CancellationToken cancel);
 
@@ -20,12 +23,16 @@ public interface IPaymentStripeClient
     Task<GetAccountsResponse> GetAccountsAsync(
         [HeaderValue(PaymentStripeConstans.PropertyOwnerIdHeaderName)]
         int propertyOwnerId,
+        [HeaderValue(PaymentStripeConstans.SubOwnerIdHeaderName)]
+        int? subOwnerId,
         CancellationToken cancel);
 
     [ApiEndpoint("POST", "/api/v1/account-sessions", Name = "CreateAccountSession")]
     Task<CreateAccountSessionResponse> CreateAccountSessionAsync(
         [HeaderValue(PaymentStripeConstans.PropertyOwnerIdHeaderName)]
         int propertyOwnerId,
+        [HeaderValue(PaymentStripeConstans.SubOwnerIdHeaderName)]
+        int? subOwnerId,
         [RequestBody] CreateAccountSessionRequest request,
         CancellationToken cancel);
 }
